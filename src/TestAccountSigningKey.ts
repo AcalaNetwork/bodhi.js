b6bbe86f01e03d0a14a1d72b3d92c429862121ab
Nantian <nxiiun@gmail.com>
2021-02-02 15:48:14 +1300

    add testaccountsigningkey

---
+++ src/TestAccountSigningKey.ts
@@ -0,0 +1,47 @@
+import type {
+  SigningKey,
+  SigningKeyResult,
+  SigningPayloadJSON
+} from './SigningKey';
+import type { KeyringOptions, KeyringPair } from '@polkadot/keyring/types';
+import { createTestPairs } from '@polkadot/keyring/testingPairs';
+
+import type { Registry } from '@polkadot/types/types';
+
+let id = 0;
+
+export class AccountSigningKey implements SigningKey {
+  #keyringPairs: KeyringPair[];
+  readonly #registry: Registry;
+
+  constructor(registry: Registry, options?: KeyringOptions, isDerived = true) {
+    const keypairMap = createTestPairs(options, isDerived);
+
+    this.#keyringPairs = Object.values(keypairMap);
+    this.#registry = registry;
+  }
+
+  public async signPayload(
+    payload: SigningPayloadJSON
+  ): Promise<SigningKeyResult> {
+    const findKeyringPair = this.#keyringPairs.find(
+      (pair) => pair.address === payload.address
+    );
+
+    if (!findKeyringPair) {
+      throw new Error(`Can't find the keyringpair for ${payload.address}`);
+    }
+
+    return new Promise((resolve): void => {
+      const signed = this.#registry
+        .createType('ExtrinsicPayload', payload, { version: payload.version })
+        .sign(findKeyringPair);
+
+      resolve({ id: ++id, ...signed });
+    });
+  }
+
+  public addKeyringPair(keyringPair: KeyringPair): void {
+    this.#keyringPairs = this.#keyringPairs.concat(keyringPair);
+  }
+}
