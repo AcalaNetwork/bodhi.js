ace4b88a0608beb6e9a0b2f027b450ec850e8c31
Nantian <nxiiun@gmail.com>
2021-03-15 12:20:21 +1300

    fix

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ const config = require('@open-web3/dev-config/config/jest.cjs');
 
 module.exports = Object.assign({}, config, {
   modulePathIgnorePatterns: ['<rootDir>/build'],
-  resolver: '@open-web3/dev-config/config/jest-resolver.cjs'
+  resolver: '@open-web3/dev-config/config/jest-resolver.cjs',
+  transformIgnorePatterns: [
+    '/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)'
+  ]
 });
