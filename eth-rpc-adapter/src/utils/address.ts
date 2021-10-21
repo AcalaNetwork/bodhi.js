d299627363618a08352036cb617da67649f36b9d
Nantian <nxiiun@gmail.com>
2021-10-21 20:42:48 +1300

    fix

--- eth-rpc-adapter/src/utils/address.ts
+++
@@ -1,6 +0,0 @@
-import { encodeAddress } from '@polkadot/util-crypto';
-import { hexToU8a, stringToU8a, u8aConcat } from '@polkadot/util';
-
-export const evmAddressToSubstrateAddress = (address: string): string => {
-  return encodeAddress(u8aConcat(stringToU8a('evm:'), hexToU8a(address), new Uint8Array(8).fill(0)));
-};
