e0fd9535f244660ccd5b78cfc47ca93c99ca4d7a
Nantian <nxiiun@gmail.com>
2021-10-28 13:56:04 +1300

    move transactionReceipt to providers (#71)

---
+++ eth-providers/src/utils/getPartialLogs.ts
@@ -0,0 +1,23 @@
+import type { EvmLog } from '@polkadot/types/interfaces';
+
+export interface PartialLog {
+  removed: boolean;
+  address: string;
+  data: string;
+  topics: string[];
+  logIndex: number;
+}
+
+export const getPartialLog = (evmLog: EvmLog, logIndex: number): PartialLog => {
+  return {
+    removed: false,
+    address: evmLog.address.toString().toLowerCase(),
+    data: evmLog.address.toString().toLowerCase(),
+    topics: evmLog.topics.toJSON() as any,
+    logIndex: logIndex
+  };
+};
+
+export const getPartialLogs = (evmLogs: EvmLog[]): PartialLog[] => {
+  return evmLogs.map((log, index) => getPartialLog(log, index));
+};
