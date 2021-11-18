893f3ac01a3539abd6300f63cfadc1a8cb567d1b
Nantian <7029338+ntduan@users.noreply.github.com>
2021-11-19 12:59:54 +1300

    fix

--- eth-transactions/src/__tests__/transaction.test.ts
+++ eth-transactions/src/__tests__/transaction.test.ts
@@ -3,31 +3,6 @@ import { expect } from 'chai';
 import { parseTransaction } from '../parseTransaction';
 import { serializeTransaction } from '../serializeTransaction';
 
-const types = {
-  Transaction: [
-    { name: 'action', type: 'string' },
-    { name: 'to', type: 'address' },
-    { name: 'nonce', type: 'uint256' },
-    { name: 'tip', type: 'uint256' },
-    { name: 'data', type: 'bytes' },
-    { name: 'value', type: 'uint256' },
-    { name: 'gasLimit', type: 'uint256' },
-    { name: 'storageLimit', type: 'uint256' },
-    { name: 'validUntil', type: 'uint256' }
-  ]
-};
-
-const value = {
-  action: 'Create',
-  to: '0x0000000000000000000000000000000000000000',
-  nonce: 0,
-  tip: 2,
-  data: '0x1111',
-  value: 0,
-  gasLimit: 2100000,
-  storageLimit: 20000,
-  validUntil: 111000
-};
 const sig =
   '0xe9df6df3d6387a9e9357a2861ebe596925fd6f7cc7d582ba787eb08e84fa945f7caaea2f24281a1fe3e087b40ac03626ba540085b78917f58c5aff80fb4e782a1c';
 
