f55192679f6d7f1ea0330d1c4b5d51ee0fb51e31
Jan Turk <jan.turk12@gmail.com>
2021-12-16 12:37:43 +0100

    Remove the preexisting truffle/ directory

--- examples/truffle/migrations/1637018277_deploy_hello_world.js
+++
@@ -1,9 +0,0 @@
-const HelloWorld = artifacts.require('HelloWorld');
-
-module.exports = async function (deployer) {
-  console.log('Deploying HelloWorld');
-
-  await deployer.deploy(HelloWorld);
-
-  console.log('HelloWorld deployed at:', HelloWorld.address);
-};
