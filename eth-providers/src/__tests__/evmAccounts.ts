a674dad59200b9e36e5acb058f02260a46f3398b
Nantian <7029338+ntduan@users.noreply.github.com>
2022-01-17 11:45:26 +1300

    Add test cases (#231)
    * add test cases
    
    * remove only
    
    * fix tests

--- eth-providers/src/__tests__/evmAccounts.ts
+++ eth-providers/src/__tests__/evmAccounts.ts
@@ -8,5 +8,15 @@ export default [
     privateKey: '0x4daddf7d5d2a9059e8065cb3ec50beabe2c23c7d6b3e380c1de8c40269acd85c',
     evmAddress: '0xb00cB924ae22b2BBb15E10c17258D6a2af980421',
     defaultSubstrateAddress: '5EMjsczwT7eJ4qYuJVQQgVYdkTArXmeLZbm9JM1QzQeQvGbi'
+  },
+  {
+    privateKey: '0xa872f6cbd25a0e04a08b1e21098017a9e6194d101d75e13111f71410c59cd57f',
+    evmAddress: '0x75E480dB528101a381Ce68544611C169Ad7EB342',
+    defaultSubstrateAddress: '5EMjsczjoEZaNbWzoXDcZtZDSHN1SLmu4ArJcEJVorNDfUH3'
+  },
+  {
+    privateKey: '0x01392cd1a09fc0f4857742f0f0daa3ebd5a0f44a7dab48c23ccd331717b97b10',
+    evmAddress: '0x0085560b24769dAC4ed057F1B2ae40746AA9aAb6',
+    defaultSubstrateAddress: '5EMjsczLHBZqbroMR4nZH6NvQQWHsVRFU9fbwQ14vBQCQx9H'
   }
 ];
