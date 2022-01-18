94eb5d9f93da6ac974a20f3ecece05e702a9a5a3
Nantian <7029338+ntduan@users.noreply.github.com>
2022-01-19 11:54:50 +1300

    claim signature (#244)
    * claim signature
    
    * export claim function

---
+++ eth-transactions/src/createClaimPayload.ts
@@ -0,0 +1,60 @@
+import { hexlify } from '@ethersproject/bytes';
+import { decodeAddress } from '@polkadot/util-crypto';
+import { logger } from './logger';
+import { ClaimPayload } from './types';
+
+export const createClaimPayload = (tx: ClaimPayload) => {
+  if (!tx.salt) {
+    return logger.throwError('claim payload missing salt');
+  }
+
+  if (!tx.chainId) {
+    return logger.throwError('claim payload missing chainId');
+  }
+
+  if (!tx.substrateAddress) {
+    return logger.throwError('claim payload missing substrateAddress');
+  }
+
+  let publicKey: Uint8Array;
+
+  try {
+    publicKey = decodeAddress(tx.substrateAddress);
+  } catch {
+    return logger.throwError('invalid substrateAddress');
+  }
+
+  return {
+    types: {
+      EIP712Domain: [
+        {
+          name: 'name',
+          type: 'string'
+        },
+        {
+          name: 'version',
+          type: 'string'
+        },
+        {
+          name: 'chainId',
+          type: 'uint256'
+        },
+        {
+          name: 'salt',
+          type: 'bytes32'
+        }
+      ],
+      Transaction: [{ name: 'substrateAddress', type: 'bytes' }]
+    },
+    primaryType: 'Transaction' as const,
+    domain: {
+      name: 'Acala EVM claim',
+      version: '1',
+      chainId: tx.chainId,
+      salt: hexlify(tx.salt)
+    },
+    message: {
+      substrateAddress: publicKey
+    }
+  };
+};
