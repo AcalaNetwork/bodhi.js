fb6cc5f0c07c0442d5250c41c44865f86b1c867c
Shunji Zhan <shunhahaha@live.com>
2022-03-29 12:40:37 +0800

    Standardized Contract Deployment/Call Process (#323)
    * test
    
    * fix
    
    * test normal sealing
    
    * polish
    
    * test normal sealing truffle
    
    * Revert "test normal sealing truffle"
    
    This reverts commit 3f377e52779e90818b314d4dca5948bcff679139.
    
    * remove loop
    
    * added hardhat evm tutorial
    
    * update lock
    
    * added hardhat EVM example to docker
    
    * temp disable EVM tests
    
    * polish
    
    * polish
    
    * remove obsolete tests
    
    * polish

--- eth-providers/src/utils/utils.ts
+++ eth-providers/src/utils/utils.ts
@@ -1,3 +1,7 @@
-export const toHex = (x: number): string => `0x${x.toString(16)}`;
+import { Extrinsic } from "@polkadot/types/interfaces";
 
 export const sleep = (interval = 1000): Promise<null> => new Promise(resolve => setTimeout(() => resolve(null), interval));
+
+export const isEVMExtrinsic = (e: Extrinsic): boolean => (
+  e.method.section.toUpperCase() === "EVM"
+);
