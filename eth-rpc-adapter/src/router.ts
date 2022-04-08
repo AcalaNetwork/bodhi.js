b5673c540cbc62a96ddcf2eea355b32955db0a23
zjb0807 <zjb0807@qq.com>
2022-04-08 08:07:26 +0800

    Return error message instead of index (#347)
    * return error instead of index
    
    * add docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Shunji Zhan <shunhahaha@live.com>
    
    Co-authored-by: Shunji Zhan <shunhahaha@live.com>

--- eth-rpc-adapter/src/router.ts
+++ eth-rpc-adapter/src/router.ts
@@ -4,6 +4,8 @@ import { InvalidParams, JSONRPCError, MethodNotFound } from './errors';
 import { logger } from './logger';
 import { JSONRPCResponse } from './transports/types';
 
+const errorRegex = /execution fatal: Module { index: (\d+), error: (\d+), message: None }/
+
 export class Router {
   readonly #bridge: Eip1193Bridge;
 
@@ -40,7 +42,14 @@ export class Router {
 
       logger.error({ err, methodName, params }, 'request error');
 
-      return { error: { code: 6969, message: `Error: ${err.message}` } };
+      let message = err.message;
+      const match = message.match(errorRegex);
+      if (match) {
+        const error = this.#bridge.provider.api.registry.findMetaError(new Uint8Array([match[1], match[2]]));
+        message = `${error.section}.${error.name}: ${error.docs}`;
+      }
+
+      return { error: { code: 6969, message: `Error: ${message}` } };
     }
   }
 
