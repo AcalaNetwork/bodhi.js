56ff0e329bfbe0e926d9cb7621546b79709f2ca9
Shunji Zhan <shunhahaha@live.com>
2022-04-26 15:29:15 +0800

    add scripts to get precompile token info (#395)
    * scripts wip
    
    * test precompile works
    
    * update
    
    * update
    
    * update

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+# Bodhi Scripts
+some util scripts for bodhi
+
+- install deps: `yarn`
+
+### Run
+- bump patch version: `yarn bump`
+- get precompile contracts info (token, oracle, EVM): `[RPC_URL=xxx] yarn precompile`
---
+++ scripts/test-precompile.ts
@@ -0,0 +1,147 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { parseUnits, Interface } from 'ethers/lib/utils';
+import tokenAbi from '@acala-network/contracts/build/contracts/Token.json';
+import evmAbi from '@acala-network/contracts/build/contracts/EVM.json';
+import oracleAbi from '@acala-network/contracts/build/contracts/Oracle.json';
+import ADDRESS from '@acala-network/contracts/utils/Address';
+
+dotenv.config();
+
+const MANDALA_TESTNET_RPC = 'https://tc7-eth.aca-dev.network';
+const RPC_URL = process.env.RPC_URL || MANDALA_TESTNET_RPC;
+
+const rpcGet =
+  (method: string) =>
+  (params: any): any =>
+    axios.get(RPC_URL, {
+      data: {
+        id: 0,
+        jsonrpc: '2.0',
+        method,
+        params
+      }
+    });
+
+const eth_call = rpcGet('eth_call');
+const eth_blockNumber = rpcGet('eth_blockNumber');
+
+const TOKENS = [
+  'ACA',
+  'AUSD',
+  'DOT',
+  'LDOT',
+  'RENBTC',
+  'CASH',
+  'KAR',
+  'KUSD',
+  'KSM',
+  'LKSM',
+  'TAI',
+  'BNC',
+  'VSKSM',
+  'PHA',
+  'KINT',
+  'KBTC'
+];
+const iface = new Interface(tokenAbi.abi);
+const ifaceEVM = new Interface(evmAbi.abi);
+const ifaceOracle = new Interface(oracleAbi.abi);
+
+const getAllTokenInfo = async () => {
+  const funcs = ['name', 'symbol', 'decimals', 'totalSupply'];
+  const queries = [];
+  const allInfo = {};
+
+  const blockNumber = (await eth_blockNumber()).data.result;
+
+  for (const token of TOKENS) {
+    allInfo[token] = { token };
+
+    queries.push(
+      ...funcs.map(async (f) => {
+        const data = iface.encodeFunctionData(f);
+        try {
+          const res = await eth_call([
+            {
+              to: ADDRESS[token],
+              data
+            },
+            blockNumber
+          ]);
+
+          const decodedData = iface.decodeFunctionResult(f, res.data.result)[0];
+          allInfo[token][f] = decodedData._isBigNumber ? decodedData.toBigInt() : decodedData;
+        } catch (error) {
+          console.log(error);
+          allInfo[token][f] = 'failed to fetch';
+        }
+      })
+    );
+  }
+
+  await Promise.all(queries);
+  console.log(allInfo);
+};
+
+const getDEXInfo = async () => {
+  const funcs = ['newContractExtraBytes', 'storageDepositPerByte', 'developerDeposit', 'publicationFee'];
+  const allInfo = {};
+
+  const blockNumber = (await eth_blockNumber()).data.result;
+
+  const queries = funcs.map(async (f) => {
+    const data = ifaceEVM.encodeFunctionData(f);
+    try {
+      const res = await eth_call([
+        {
+          to: ADDRESS.EVM,
+          data
+        },
+        blockNumber
+      ]);
+
+      const decodedData = ifaceEVM.decodeFunctionResult(f, res.data.result)[0];
+      allInfo[f] = decodedData._isBigNumber ? decodedData.toBigInt() : decodedData;
+    } catch (error) {
+      console.log(error);
+      allInfo[f] = 'failed to fetch';
+    }
+  });
+
+  await Promise.all(queries);
+  console.log({ DEX: allInfo });
+};
+
+const getOracleInfo = async () => {
+  const allInfo = {};
+
+  const blockNumber = (await eth_blockNumber()).data.result;
+
+  const queries = TOKENS.map(async (name) => {
+    const addr = ADDRESS[name];
+    const data = ifaceOracle.encodeFunctionData('getPrice', [addr]);
+    try {
+      const res = await eth_call([
+        {
+          to: ADDRESS.Oracle,
+          data
+        },
+        blockNumber
+      ]);
+
+      const decodedData = ifaceOracle.decodeFunctionResult('getPrice', res.data.result)[0];
+      allInfo[name] = decodedData._isBigNumber ? decodedData.toBigInt() : decodedData;
+    } catch (error) {
+      console.log(error);
+      allInfo[name] = 'failed to fetch';
+    }
+  });
+
+  await Promise.all(queries);
+  console.log({ OraclePrices: allInfo });
+};
+
+getAllTokenInfo();
+getDEXInfo();
+getOracleInfo();
