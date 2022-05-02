b2ebf1783eb7b081d232b772b9bbb9a19f5df7a4
Nantian <7029338+ntduan@users.noreply.github.com>
2022-05-03 10:29:49 +1200

    delete used transport

--- eth-rpc-adapter/src/transports/index.ts
+++ eth-rpc-adapter/src/transports/index.ts
@@ -1,28 +1,20 @@
 import WebSocketTransport, { WebSocketServerTransportOptions } from './websocket';
 import HTTPTransport, { HTTPServerTransportOptions } from './http';
-import HTTPSTransport, { HTTPSServerTransportOptions } from './https';
-import IPCTransport, { IPCServerTransportOptions } from './ipc';
 import { ServerTransport } from './server-transport';
-export { HTTPSTransport, HTTPTransport, WebSocketTransport, IPCTransport, ServerTransport };
+export { HTTPTransport, WebSocketTransport, ServerTransport };
 
 export type TransportNames = 'IPCTransport' | 'HTTPTransport' | 'HTTPSTransport' | 'WebSocketTransport';
 
-export type TransportClasses = WebSocketTransport | HTTPTransport | HTTPSTransport | IPCTransport | ServerTransport;
+export type TransportClasses = WebSocketTransport | HTTPTransport | ServerTransport;
 
-export type TransportOptions =
-  | WebSocketServerTransportOptions
-  | HTTPServerTransportOptions
-  | HTTPSServerTransportOptions
-  | IPCServerTransportOptions;
+export type TransportOptions = WebSocketServerTransportOptions | HTTPServerTransportOptions;
 
 export interface TransportsMapping {
   [name: string]: any;
 }
 
 const transports: TransportsMapping = {
-  HTTPSTransport,
   HTTPTransport,
-  IPCTransport,
   WebSocketTransport
 };
 
