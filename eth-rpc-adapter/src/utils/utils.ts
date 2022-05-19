eadcd20dd3077544088ad48c11b73d78b79670c2
HussainMehdi <csmehdi101@gmail.com>
2022-05-19 14:53:50 +0500

    * Added datadog-util.ts * Add flattened dd tags instead of nested

--- eth-rpc-adapter/src/utils/utils.ts
+++ eth-rpc-adapter/src/utils/utils.ts
@@ -1,1 +1,6 @@
+import { assignTracerSpan, buildTracerSpan } from './datadog-util';
 export const sleep = async (time: number = 1000): Promise<void> => new Promise((resolve) => setTimeout(resolve, time));
+export const DataDogUtil = {
+  buildTracerSpan,
+  assignTracerSpan
+};
