fab77e82e0ec8291fcec6af4029647d96e38d8d2
Shunji Zhan <shunhahaha@live.com>
2022-09-23 15:35:13 +0800

    wip

--- eth-rpc-adapter/src/index.ts
+++ eth-rpc-adapter/src/index.ts
@@ -1,5 +1,84 @@
 import 'dd-trace/init';
-import { start } from './server';
+import { EvmRpcProvider } from '@acala-network/eth-providers';
+import HTTPServerTransport from './transports/http';
+import WebSocketServerTransport from './transports/websocket';
+import { Eip1193Bridge } from './eip1193-bridge';
+import { RpcForward } from './rpc-forward';
+import { Router } from './router';
+import { version } from './_version';
+import { parseOptions } from './utils';
+
+import EthRpcServer from './server';
+
+export async function start(): Promise<void> {
+  console.log('starting server ...');
+
+  const opts = parseOptions();
+  const provider = EvmRpcProvider.from(opts.endpoints.split(','), {
+    safeMode: opts.safeMode,
+    localMode: opts.localMode,
+    richMode: opts.richMode,
+    verbose: opts.verbose,
+    subqlUrl: opts.subqlUrl,
+    maxBlockCacheSize: opts.maxBlockCacheSize,
+    storageCacheSize: opts.storageCacheSize
+  });
+
+  const bridge = new Eip1193Bridge(provider);
+
+  // const rpcForward = opts.forwardMode ? new RpcForward(provider) : undefined;
+
+  const router = new Router(bridge);
+
+  const server = new EthRpcServer({
+    port: opts.httpPort,
+    middleware: [],
+    batchSize: opts.maxBatchSize
+  });
+
+  // const WebSocketTransport = new WebSocketServerTransport({
+  //   port: opts.wsPort,
+  //   middleware: [],
+  //   batchSize: opts.maxBatchSize
+  // });
+
+  server.addRouter(router as any);
+  // WebSocketTransport.addRouter(router as any);
+
+  await provider.isReady();
+
+  server.start();
+  // WebSocketTransport.start();
+
+  if (provider.subql) {
+    console.log('!!!!!');
+    await provider.subql?.checkGraphql();
+  }
+
+  // init rpc methods
+  // if (rpcForward) {
+  //   await rpcForward.initRpcMethods();
+  // }
+
+  console.log(`
+  --------------------------------------------
+               🚀 SERVER STARTED 🚀
+  --------------------------------------------
+  version         : ${version}
+  endpoint url    : ${opts.endpoints}
+  subquery url    : ${opts.subqlUrl}
+  listening to    : http ${opts.httpPort} | ws ${opts.wsPort}
+  max blockCache  : ${opts.maxBlockCacheSize}
+  max batchSize   : ${opts.maxBatchSize}
+  max storageSize : ${opts.storageCacheSize}
+  safe mode       : ${opts.safeMode}
+  local mode      : ${opts.localMode}
+  forward mode    : ${opts.forwardMode}
+  rich mode       : ${opts.richMode}
+  verbose         : ${opts.verbose}
+  --------------------------------------------
+  `);
+}
 
 start().catch((e) => {
   console.error(e);
--- eth-rpc-adapter/src/server.ts
+++ eth-rpc-adapter/src/server.ts
@@ -1,77 +1,143 @@
-import { EvmRpcProvider } from '@acala-network/eth-providers';
-import HTTPServerTransport from './transports/http';
-import WebSocketServerTransport from './transports/websocket';
-import { Eip1193Bridge } from './eip1193-bridge';
-import { RpcForward } from './rpc-forward';
+import { json as jsonParser } from 'body-parser';
+import connect, { HandleFunction } from 'connect';
+import cors from 'cors';
+import http, { ServerOptions } from 'http';
+import { logger } from './logger';
+import { errorHandler } from './middlewares';
+import { BatchSizeError } from './errors';
+
+import WebSocket from 'ws';
+import { InvalidRequest, MethodNotFound } from './errors';
 import { Router } from './router';
-import { version } from './_version';
-import { parseOptions } from './utils';
+import { DataDogUtil } from './utils';
+import type { JSONRPCRequest, JSONRPCResponse } from './transports/types';
 
-export async function start(): Promise<void> {
-  console.log('starting server ...');
+export interface EthRpcServerOptions extends ServerOptions {
+  middleware: HandleFunction[];
+  port: number;
+  cors?: cors.CorsOptions;
+  batchSize: number;
+}
 
-  const opts = parseOptions();
-  const provider = EvmRpcProvider.from(opts.endpoints.split(','), {
-    safeMode: opts.safeMode,
-    localMode: opts.localMode,
-    richMode: opts.richMode,
-    verbose: opts.verbose,
-    subqlUrl: opts.subqlUrl,
-    maxBlockCacheSize: opts.maxBlockCacheSize,
-    storageCacheSize: opts.storageCacheSize
-  });
+export default class EthRpcServer {
+  private static defaultCorsOptions = { origin: '*' };
+  private server: http.Server;
+  private options: EthRpcServerOptions;
 
-  const bridge = new Eip1193Bridge(provider);
+  public routers: Router[] = [];
 
-  const rpcForward = opts.forwardMode ? new RpcForward(provider) : undefined;
+  constructor(options: EthRpcServerOptions) {
+    const app = connect();
 
-  const router = new Router(bridge, rpcForward);
+    const corsOptions = options.cors || EthRpcServer.defaultCorsOptions;
+    this.options = {
+      ...options,
+      middleware: [
+        cors(corsOptions) as HandleFunction,
+        jsonParser({
+          limit: '1mb'
+        }),
+        ...options.middleware
+      ]
+    };
 
-  const HTTPTransport = new HTTPServerTransport({
-    port: opts.httpPort,
-    middleware: [],
-    batchSize: opts.maxBatchSize
-  });
+    this.options.middleware.forEach((mw) => app.use(mw));
 
-  const WebSocketTransport = new WebSocketServerTransport({
-    port: opts.wsPort,
-    middleware: [],
-    batchSize: opts.maxBatchSize
-  });
+    app.use(this.httpRouterHandler.bind(this) as HandleFunction);
+    app.use(errorHandler);
 
-  HTTPTransport.addRouter(router as any);
-  WebSocketTransport.addRouter(router as any);
-
-  await provider.isReady();
-
-  HTTPTransport.start();
-  WebSocketTransport.start();
-
-  if (provider.subql) {
-    await provider.subql?.checkGraphql();
+    this.server = http.createServer(app);
   }
 
-  // init rpc methods
-  if (rpcForward) {
-    await rpcForward.initRpcMethods();
+  public start(): void {
+    this.server.listen(this.options.port);
   }
 
-  console.log(`
-  --------------------------------------------
-               🚀 SERVER STARTED 🚀
-  --------------------------------------------
-  version         : ${version}
-  endpoint url    : ${opts.endpoints}
-  subquery url    : ${opts.subqlUrl}
-  listening to    : http ${opts.httpPort} | ws ${opts.wsPort}
-  max blockCache  : ${opts.maxBlockCacheSize}
-  max batchSize   : ${opts.maxBatchSize}
-  max storageSize : ${opts.storageCacheSize}
-  safe mode       : ${opts.safeMode}
-  local mode      : ${opts.localMode}
-  forward mode    : ${opts.forwardMode}
-  rich mode       : ${opts.richMode}
-  verbose         : ${opts.verbose}
-  --------------------------------------------
-  `);
+  public stop(): void {
+    this.server.close();
+  }
+
+  public addRouter(router: Router): void {
+    this.routers.push(router);
+  }
+
+  public removeRouter(router: Router): void {
+    this.routers = this.routers.filter((r) => r !== router);
+  }
+
+  protected async routerHandler({ id, method, params }: JSONRPCRequest, ws?: WebSocket): Promise<JSONRPCResponse> {
+    let res: JSONRPCResponse = {
+      id: id ?? null,
+      jsonrpc: '2.0'
+    };
+
+    if (id === null || id === undefined || !method) {
+      return {
+        ...res,
+        error: new InvalidRequest({
+          id: id || null,
+          method: method || null,
+          params: params || null
+        }).json()
+      };
+    }
+
+    if (this.routers.length === 0) {
+      logger.warn('transport method called without a router configured.'); // tslint:disable-line
+      throw new Error('No router configured');
+    }
+
+    // Initialize datadog span and get spanTags from the context
+    const spanTags = DataDogUtil.buildTracerSpan();
+
+    const routerForMethod = this.routers.find((r) => r.isMethodImplemented(method));
+
+    if (routerForMethod === undefined) {
+      res = {
+        ...res,
+        // method not found in any of the routers.
+        error: new MethodNotFound('Method not found', `The method ${method} does not exist / is not available.`).json()
+      };
+    } else {
+      res = {
+        ...res,
+        ...(await routerForMethod.call(method, params as any, ws))
+      };
+    }
+    // Add span tags to the datadog span
+    DataDogUtil.assignTracerSpan(spanTags, {
+      id,
+      method,
+      ...(Array.isArray(params)
+        ? params.reduce((c, v, i) => {
+            return { ...c, [`param_${i}`]: v };
+          }, {})
+        : params)
+    });
+
+    return res;
+  }
+
+  private async httpRouterHandler(req: any, res: http.ServerResponse, next: (err?: any) => void): Promise<void> {
+    logger.debug(req.body, 'incoming request');
+    let result = null;
+    if (req.body instanceof Array) {
+      if (req.body.length > this.options.batchSize) {
+        return next(new BatchSizeError(this.options.batchSize, req.body.length));
+      } else {
+        result = await Promise.all(req.body.map((r: JSONRPCRequest) => this.routerHandler(r)));
+      }
+    } else {
+      try {
+        result = await this.routerHandler(req.body);
+      } catch (e) {
+        return next(e);
+      }
+    }
+
+    logger.debug(result, 'request completed');
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+  }
 }
