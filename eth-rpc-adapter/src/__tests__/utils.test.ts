c65f9c3325abefc7f764f337719efd05cc4f6b96
Shunji Zhan <shunhahaha@live.com>
2022-10-26 15:55:59 +0800

    fix env issue (#585)

---
+++ eth-rpc-adapter/src/__tests__/utils.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { parseBooleanOption } from '../utils';
+
+describe('parseBooleanOption', () => {
+  it('parse correctly', () => {
+    expect(parseBooleanOption('xxx', true, undefined)).to.equal(true);
+    expect(parseBooleanOption('xxx', false, undefined)).to.equal(false);
+
+    expect(parseBooleanOption('xxx', true, '0')).to.equal(false);
+    expect(parseBooleanOption('xxx', true, '1')).to.equal(true);
+
+    expect(parseBooleanOption('xxx', false, '0')).to.equal(false);
+    expect(parseBooleanOption('xxx', false, '1')).to.equal(true);
+
+    expect(parseBooleanOption('xxx', true, 'false')).to.equal(false);
+    expect(parseBooleanOption('xxx', true, 'true')).to.equal(true);
+
+    expect(parseBooleanOption('xxx', false, 'false')).to.equal(false);
+    expect(parseBooleanOption('xxx', false, 'true')).to.equal(true);
+
+    expect(() => parseBooleanOption('xxx', false, 'hahaha')).to.throw(
+      `boolean env xxx should be any of { true, false, 1, 0 }, got hahaha`
+    );
+  });
+});
