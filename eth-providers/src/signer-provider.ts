05d652b70169bc1fce6ce3ad28845933d54e64a8
Shunji Zhan <shunhahaha@live.com>
2022-11-30 16:56:57 +0800

    fix

--- eth-providers/src/signer-provider.ts
+++ eth-providers/src/signer-provider.ts
@@ -2,11 +2,17 @@ import { ApiPromise } from '@polkadot/api';
 import type { ApiOptions } from '@polkadot/api/types';
 import { options } from '@acala-network/api';
 import { BaseProvider } from './base-provider';
+import { extraRuntimeTypes } from './utils';
 
 export class SignerProvider extends BaseProvider {
   constructor(apiOptions: ApiOptions) {
     super();
-    const api = new ApiPromise(options(apiOptions));
+    const api = new ApiPromise(
+      options({
+        ...apiOptions,
+        types: extraRuntimeTypes
+      })
+    );
     this.setApi(api);
   }
 
