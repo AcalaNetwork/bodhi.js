bb7e2cf4202cbe297f78ed6535c9b0493b8b3fce
Shunji Zhan <shunhahaha@live.com>
2022-12-29 17:23:14 +0800

    update

--- examples/chopsticks/index.ts
+++ examples/chopsticks/index.ts
@@ -1,16 +1,10 @@
 import { Contract, Wallet } from 'ethers';
 import { EvmRpcProvider } from '@acala-network/eth-providers';
-import echoMeta from './Echo.json';
+import echoJson from './Echo.json';
 
 const ECHO_ADDRESS = '0xe8669bfe6fe29dde37d670fbc1cf96365025c242';
-const ECHO_ABI = [
-  'function scream(string memory message) public returns(string memory)',
-  'function echo() public returns(string)'
-];
-
 const PRIVATE_KEY = '0x8d2d614677b99ee1809eec0967d538f43d3f410e20ee5f5b979dd21d5930d3fe';
 const ETH_ADDRESS = '0xEE1b6e72FC5bC8738150B6bE7564DA887723cCA1';
-const RANDOM_ADDRESS_SUBSTRATE = '5GqBr7hDMWzmd1DS8ruDCweRDUbDDvFT37Bwk8tocTF9RzHF';
 
 const LOCAL_NODE_URL = 'ws://localhost:8000';
 const MANDALA_NODE_URL = 'wss://mandala-rpc.aca-staging.network/ws';
@@ -20,8 +14,8 @@ const main = async () => {
   const providerLocal = new EvmRpcProvider(LOCAL_NODE_URL);
   const signerLocal = new Wallet(PRIVATE_KEY, providerLocal);
 
-  const echoMandala = new Contract(ECHO_ADDRESS, echoMeta.abi, providerMandala);
-  const echoLocal = new Contract(ECHO_ADDRESS, echoMeta.abi, signerLocal);
+  const echoMandala = new Contract(ECHO_ADDRESS, echoJson.abi, providerMandala);
+  const echoLocal = new Contract(ECHO_ADDRESS, echoJson.abi, signerLocal);
 
   const _printState = async (stateName: string) => {
     const msgMandala = await echoMandala.callStatic.echo();
