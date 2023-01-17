271e0db4b29ab654209b3c3f39b1b8061faf9552
Shunji Zhan <shunhahaha@live.com>
2023-01-17 19:10:04 +0800

    bump v2.5.10

--- bodhi/src/_version.ts
+++ bodhi/src/_version.ts
@@ -1,1 +1,1 @@
-export const version: string = 'bodhi.js/bodhi/2.5.9';
+export const version: string = 'bodhi.js/bodhi/2.5.10';
--- eth-transactions/src/_version.ts
+++ eth-transactions/src/_version.ts
@@ -1,1 +1,1 @@
-export const version: string = 'bodhi.js/transactions/2.5.9';
+export const version: string = 'bodhi.js/transactions/2.5.10';
