d0b189341de7164a234123a566939a83615e48d8
Shunji Zhan <shunhahaha@live.com>
2023-02-19 13:08:15 +0800

    remove extra delete

--- eth-rpc-adapter/src/eip1193-bridge.ts
+++ eth-rpc-adapter/src/eip1193-bridge.ts
@@ -337,8 +337,6 @@ class Eip1193BridgeImpl {
     if (!res) return null;
 
     // @ts-ignore
-    delete res.byzantium;
-    // @ts-ignore
     delete res.confirmations;
     return hexlifyRpcResult(res);
   }
