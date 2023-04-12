511454805904c72a01d9c04429b034e97cbf3c95
Ermal Kaleci <ermalkaleci@gmail.com>
2023-04-13 00:44:15 +0200

    fix e2e test

--- packages/eth-providers/vitest.config.ts
+++ packages/eth-providers/vitest.config.ts
@@ -1,7 +1,18 @@
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
-    exclude: [],
+    testTimeout: 300000,
+    hookTimeout: 60000,
+    dangerouslyIgnoreUnhandledErrors: true,
+    threads: false,
+    coverage: {
+      provider: 'c8', // or 'istanbul'
+      reporter: ['text', 'json', 'html'],
+    },
+    include: ['src/**/*.test.ts'],
   },
+  plugins: [swc.vite(), tsconfigPaths()],
 });
