b870995a2c8a153876a93614c35f496b7e92707f
Shunji Zhan <shunhahaha@live.com>
2023-05-05 12:18:04 +0800

    use latest types, and temp patch runtime txPayment call

--- packages/eth-providers/src/rpc-provider.ts
+++ packages/eth-providers/src/rpc-provider.ts
@@ -1,13 +1,21 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { BaseProvider, BaseProviderOptions } from './base-provider';
 import { options } from '@acala-network/api';
+import { runtimePatch } from './utils/temp-runtime-patch';
 
 export class EvmRpcProvider extends BaseProvider {
   constructor(endpoint: string | string[], opts?: BaseProviderOptions) {
     super(opts);
 
     const provider = new WsProvider(endpoint);
-    const api = new ApiPromise(options({ provider }));
+    // const api = new ApiPromise(options({ provider }));
+    const api = new ApiPromise({
+      ...options({ provider }),
+      runtime: {
+        ...options({ provider }).runtime,
+        ...(runtimePatch as any),
+      },
+    } );
 
     this.setApi(api);
   }
