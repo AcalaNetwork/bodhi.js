e96a5bcb454c620f6d8b23cff3f75407963680d7
Shunji Zhan <shunhahaha@live.com>
2023-05-29 15:31:14 +0800

    update

--- packages/eth-providers/src/base-provider.ts
+++ packages/eth-providers/src/base-provider.ts
@@ -984,7 +984,8 @@ export abstract class BaseProvider extends AbstractProvider {
       ? BigNumber.from(tx.gasPrice)
       : await this.getGasPrice();
 
-    const isTokenTransfer = hexlify(await transaction.data ?? '').startsWith('0xa9059cbb');  // transfer(address,uint256)
+    const tokenTransferSelector = '0xa9059cbb';   // transfer(address,uint256)
+    const isTokenTransfer = hexlify(await transaction.data ?? '').startsWith(tokenTransferSelector);
     return encodeGasLimit(txFee, gasPrice, gasLimit, usedStorage, isTokenTransfer);
   };
 
