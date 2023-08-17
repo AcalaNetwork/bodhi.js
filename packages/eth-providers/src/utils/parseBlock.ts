077821365f97058d06ffc9ce8cc81f1b2da3dc0d
shunjizhan <shunhahaha@live.com>
2023-08-17 16:59:29 +0800

    fix query old block

--- packages/eth-providers/src/utils/parseBlock.ts
+++ packages/eth-providers/src/utils/parseBlock.ts
@@ -4,11 +4,12 @@ import { ApiPromise } from '@polkadot/api';
 import { BIGNUMBER_ZERO, ONE_HUNDRED_GWEI } from '../consts';
 import { BigNumber } from 'ethers';
 import {
-  DispatchInfo,
   EventRecord,
   RuntimeDispatchInfoV1,
   RuntimeDispatchInfoV2,
   SignedBlock,
+  WeightV1,
+  WeightV2,
 } from '@polkadot/types/interfaces';
 import { Formatter } from '@ethersproject/providers';
 import { FrameSupportDispatchDispatchInfo, FrameSystemEventRecord } from '@polkadot/types/lookup';
@@ -136,22 +137,19 @@ const getEffectiveGasPrice = async (
       throw new Error(`cannot find extrinsic success event: ${JSON.stringify(extrinsicEvents)}`);
     }
 
-    const dispatchInfo = successEvent.event.data[0] as FrameSupportDispatchDispatchInfo | DispatchInfo;
-
+    const dispatchInfo = successEvent.event.data[0];
     const actualWeight =
-      (dispatchInfo as FrameSupportDispatchDispatchInfo).weight.refTime ??
-      (dispatchInfo as DispatchInfo).weight.refTime;
+      ((dispatchInfo as any).weight as WeightV2).refTime ??
+      (dispatchInfo as any).weight as WeightV1;
 
     const [paymentInfo, feeDetails] = await Promise.all([
-      apiAtParentBlock.call.transactionPaymentApi.queryInfo<RuntimeDispatchInfoV1 | RuntimeDispatchInfoV2>(
-        u8a,
-        u8a.length
-      ),
+      apiAtParentBlock.call.transactionPaymentApi.queryInfo<RuntimeDispatchInfoV1 | RuntimeDispatchInfoV2>(u8a,u8a.length),
       apiAtParentBlock.call.transactionPaymentApi.queryFeeDetails(u8a, u8a.length),
     ]);
 
     const estimatedWeight =
-      (paymentInfo as RuntimeDispatchInfoV2).weight.refTime ?? (paymentInfo as RuntimeDispatchInfoV1).weight;
+      (paymentInfo as RuntimeDispatchInfoV2).weight?.refTime ??
+      (paymentInfo as RuntimeDispatchInfoV1).weight;
 
     const { baseFee, lenFee, adjustedWeightFee } = feeDetails.inclusionFee.unwrap();
 
