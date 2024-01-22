1fdb1d539d7b0ecac833f8d1df538ca3e4020e06
Shunji Zhan <shunhahaha@live.com>
2024-01-22 14:33:03 +0800

    fix types bundle

--- packages/eth-providers/src/rpc-provider.ts
+++ packages/eth-providers/src/rpc-provider.ts
@@ -1,14 +1,17 @@
-import { options } from '@acala-network/api';
-
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import { acalaRuntime, acalaTypesBundle } from '@acala-network/types';
+
 import { BaseProvider, BaseProviderOptions } from './base-provider';
 
 export class EvmRpcProvider extends BaseProvider {
   constructor(endpoint: string | string[], opts?: BaseProviderOptions) {
     super(opts);
 
-    const provider = new WsProvider(endpoint);
-    const api = new ApiPromise(options({ provider }));
+    const api = new ApiPromise({
+      provider: new WsProvider(endpoint),
+      runtime: acalaRuntime,
+      typesBundle: acalaTypesBundle,
+    });
 
     this.setApi(api);
   }
