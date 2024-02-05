faa333a97f9ed999c8439906d6b7185df4035a5f
Shunji Zhan <shunhahaha@live.com>
2024-02-05 14:39:54 +0800

    bump v2.7.20

--- packages/eth-rpc-adapter/src/_version.ts
+++ packages/eth-rpc-adapter/src/_version.ts
@@ -1,1 +1,1 @@
-export const version = 'bodhi.js/eth-rpc-adapter/2.7.19';
+export const version = 'bodhi.js/eth-rpc-adapter/2.7.20';
--- packages/eth-transactions/src/_version.ts
+++ packages/eth-transactions/src/_version.ts
@@ -1,1 +1,1 @@
-export const version = 'bodhi.js/transactions/2.7.19';
+export const version = 'bodhi.js/transactions/2.7.20';
