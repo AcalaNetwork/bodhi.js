f9931f28e95442624bb29fe36934c10bf1ad8d7c
Shunji Zhan <shunhahaha@live.com>
2024-04-05 11:15:40 +0800

    use latest subql node v3

--- packages/evm-subql/src/index.ts
+++ packages/evm-subql/src/index.ts
@@ -1,2 +1,5 @@
-//Exports all handler functions
+// https://github.com/subquery/subql/issues/1277#issuecomment-1404181415
+import { atob } from 'abab';
+global.atob = atob as any;
+
 export * from './mappings/mappingHandlers';
