399a2c84efd5705ae5532f4b3a9788eb6df58028
Shunji Zhan <shunhahaha@live.com>
2024-09-25 17:08:50 +0800

    fix merge

--- scripts/merge-coverage.sh
+++ scripts/merge-coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-yarn nyc merge packages/bodhi/coverage .nyc_output/eth-transactions.json
+yarn nyc merge packages/bodhi/coverage .nyc_output/bodhi.json
 yarn nyc merge packages/eth-transactions/coverage .nyc_output/eth-transactions.json
 yarn nyc merge packages/eth-providers/coverage .nyc_output/eth-provider.json
 yarn nyc merge packages/eth-rpc-adapter/coverage .nyc_output/eth-rpc-adapter.json
 
 yarn nyc merge packages/eth-rpc-adapter/coverage-e2e-hardhat .nyc_output/e2e-hardhat.json
 yarn nyc merge packages/eth-rpc-adapter/coverage-e2e-truffle .nyc_output/e2e-truffle.json
-
+yarn nyc merge packages/eth-rpc-adapter/coverage-e2e-viem .nyc_output/e2e-viem.json
 yarn nyc report   \
   --reporter=html \
   --reporter=lcov \
