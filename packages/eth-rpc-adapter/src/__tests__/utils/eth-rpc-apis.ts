3ed7108b11ca93d148702e4bbf121811ff08f135
Shunji Zhan <shunhahaha@live.com>
2024-10-09 11:28:43 +0800

    eth_getBlockByNumber tests

--- packages/eth-rpc-adapter/src/__tests__/utils/eth-rpc-apis.ts
+++ packages/eth-rpc-adapter/src/__tests__/utils/eth-rpc-apis.ts
@@ -48,10 +48,4 @@ export const eth_getFilterLogs = rpcGet<{
 }>('eth_getFilterLogs');
 export const eth_uninstallFilter = rpcGet('eth_uninstallFilter');
 export const net_listening = rpcGet('net_listening');
-
-/* ---------- karura mainnet rpc methods ---------- */
-export const eth_blockNumber_karura = rpcGet('eth_blockNumber', KARURA_ETH_RPC_URL);
-export const eth_getTransactionReceipt_karura = rpcGet('eth_getTransactionReceipt', KARURA_ETH_RPC_URL);
-export const eth_getTransactionByHash_karura = rpcGet('eth_getTransactionByHash', KARURA_ETH_RPC_URL);
-export const eth_getBlockByNumber_karura = rpcGet('eth_getBlockByNumber', KARURA_ETH_RPC_URL);
 export const eth_getStorageAt = rpcGet('eth_getStorageAt', ETH_RPC_URL);
