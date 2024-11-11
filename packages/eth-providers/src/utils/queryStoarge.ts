02e724d55deec38715fdf8ab80a92c35d2d09ff7
Shunji Zhan <shunhahaha@live.com>
2024-11-11 15:49:23 +0800

    update

--- packages/eth-providers/src/utils/queryStoarge.ts
+++ packages/eth-providers/src/utils/queryStoarge.ts
@@ -1,7 +1,7 @@
 import { ApiPromise } from '@polkadot/api';
 import { Codec } from '@polkadot/types/types';
 import { decorateStorage, unwrapStorageType } from '@polkadot/types';
-import { isNull, u8aToU8a } from '@polkadot/util';
+import { u8aToU8a } from '@polkadot/util';
 
 export const queryStorage = async <T extends Codec = Codec>(
   api: ApiPromise,
@@ -9,57 +9,45 @@ export const queryStorage = async <T extends Codec = Codec>(
   args: any[],
   blockHash: string,
 ): Promise<T> => {
-  const apiAt = await api.at(blockHash);
+  const registry = await api.getBlockRegistry(u8aToU8a(blockHash));
+
+  const storage = decorateStorage(
+    registry.registry,
+    registry.metadata.asLatest,
+    registry.metadata.version,
+  );
+
   const [section, method] = module.split('.');
-  const res = await apiAt.query[section][method](...args);
-  return res as T;
+
+  const entry = storage[section][method];
+  const key = entry(...args);
+
+  const outputType = unwrapStorageType(
+    registry.registry,
+    entry.meta.type,
+    entry.meta.modifier.isOptional,
+  );
+
+
+  const value: any = await api.rpc.state.getStorage(key, blockHash);
+
+  // we convert to Uint8Array since it maps to the raw encoding, all
+  // data will be correctly encoded (incl. numbers, excl. :code)
+  const input = value === null
+    ? null
+    : u8aToU8a(
+      entry.meta.modifier.isOptional
+        ? value.toU8a()
+        : value.isSome
+          ? value.unwrap().toU8a()
+          : null,
+    );
+
+
+  const result = registry.registry.createTypeUnsafe<T>(outputType, [input], {
+    blockHash,
+    isPedantic: !entry.meta.modifier.isOptional,
+  });
+
+  return result;
 };
-
-// export const queryStorage = async <T extends Codec = Codec>(
-//   api: ApiPromise,
-//   module: `${string}.${string}`,
-//   args: any[],
-//   blockHash: string,
-// ): Promise<T> => {
-//   const registry = await api.getBlockRegistry(u8aToU8a(blockHash));
-
-//   const storage = decorateStorage(
-//     registry.registry,
-//     registry.metadata.asLatest,
-//     registry.metadata.version,
-//   );
-
-//   const [section, method] = module.split('.');
-
-//   const entry = storage[section][method];
-//   const key = entry(...args);
-
-//   const outputType = unwrapStorageType(
-//     registry.registry,
-//     entry.meta.type,
-//     entry.meta.modifier.isOptional,
-//   );
-
-
-//   const value: any = await api.rpc.state.getStorage(key, blockHash);
-
-//   // we convert to Uint8Array since it maps to the raw encoding, all
-//   // data will be correctly encoded (incl. numbers, excl. :code)
-//   const input = value === null
-//     ? null
-//     : u8aToU8a(
-//       entry.meta.modifier.isOptional
-//         ? value.toU8a()
-//         : value.isSome
-//           ? value.unwrap().toU8a()
-//           : null,
-//     );
-
-
-//   const result = registry.registry.createTypeUnsafe<T>(outputType, [input], {
-//     blockHash,
-//     isPedantic: !entry.meta.modifier.isOptional,
-//   });
-
-//   return result;
-// };
