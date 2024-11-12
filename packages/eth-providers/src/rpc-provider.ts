febbf1ba34ee67f1284f0a5cb97958000796a301
Shunji Zhan <shunhahaha@live.com>
2024-11-12 14:13:41 +0800

    add option for cache capacity

--- packages/eth-providers/src/rpc-provider.ts
+++ packages/eth-providers/src/rpc-provider.ts
@@ -1,20 +1,26 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { withAcalaTypes } from '@acala-network/api';
+import { acalaTypesBundle } from '@acala-network/types';
 
 import { BaseProvider, BaseProviderOptions } from './base-provider';
 
+export type EvmRpcProviderOptions = BaseProviderOptions & {
+  rpcCacheCapacity?: number;
+};
+
 export class EvmRpcProvider extends BaseProvider {
-  constructor(endpoint: string | string[], opts?: BaseProviderOptions) {
+  constructor(endpoint: string | string[], opts?: EvmRpcProviderOptions) {
     super(opts);
 
-    const api = new ApiPromise(withAcalaTypes({
+    const api = new ApiPromise({
       provider: new WsProvider(endpoint),
-    }));
+      typesBundle: acalaTypesBundle,
+      rpcCacheCapacity: opts?.rpcCacheCapacity,
+    });
 
     this.setApi(api);
   }
 
-  static from(endpoint: string | string[], opt?: BaseProviderOptions): EvmRpcProvider {
+  static from(endpoint: string | string[], opt?: EvmRpcProviderOptions): EvmRpcProvider {
     return new EvmRpcProvider(endpoint, opt);
   }
 }
