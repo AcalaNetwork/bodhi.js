18674047adc52a36268f48784ea84bc8b2d31e3f
zjb0807 <zjb0807@qq.com>
2025-03-14 11:09:02 +0800

    Update index.ts

--- packages/eth-rpc-adapter/src/index.ts
+++ packages/eth-rpc-adapter/src/index.ts
@@ -83,3 +83,9 @@ process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
   process.exit(1);
 });
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("unhandled Promise Rejection:", reason);
+  process.exit(1);
+});
+
