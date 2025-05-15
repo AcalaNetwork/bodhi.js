f2d786cabda6648141283414bf4644a75671cf14
Bryan Chen <xlchen1291@gmail.com>
2025-05-15 12:43:40 +1200

    v2.9.6

--- packages/bodhi/src/_version.ts
+++ packages/bodhi/src/_version.ts
@@ -1,1 +1,1 @@
-export const version = 'bodhi.js/bodhi/2.9.5';
+export const version = 'bodhi.js/bodhi/2.9.6';
--- packages/eth-providers/src/_version.ts
+++ packages/eth-providers/src/_version.ts
@@ -1,1 +1,1 @@
-export const version = 'bodhi.js/providers/2.9.5';
+export const version = 'bodhi.js/providers/2.9.6';
